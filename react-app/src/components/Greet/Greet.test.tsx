183f876ff1fd5b62cf8805588351c727a0185720
Ankur Atri <ankuratri.in@gmail.com>
2024-03-24 22:59:07 +0530

    Skipping and Grouping tests example added

--- react-app/src/components/Greet/Greet.test.tsx
+++ react-app/src/components/Greet/Greet.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("renders hello world correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello world/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test.skip("renders a name", () => {
+    render(<Greet name="Ankur" />);
+    const textElement = screen.getByText("Hello Ankur");
+    expect(textElement).toBeInTheDocument();
+  });
 });
