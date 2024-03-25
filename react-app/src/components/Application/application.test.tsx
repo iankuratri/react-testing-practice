d867e362e4d660642825312fd7d13235de277cb4
Ankur Atri <ankuratri.in@gmail.com>
2024-03-25 16:06:03 +0530

    getByLabelText example and notes added

--- react-app/src/components/Application/application.test.tsx
+++ react-app/src/components/Application/application.test.tsx
@@ -18,6 +18,9 @@ describe("Application", () => {
     const nameElement = screen.getByRole("textbox", { name: "Name" });
     expect(nameElement).toBeInTheDocument();
 
+    const nameElement2 = screen.getByLabelText("Name", { selector: "input" });
+    expect(nameElement2).toBeInTheDocument();
+
     const bioElement = screen.getByRole("textbox", { name: "Bio" });
     expect(bioElement).toBeInTheDocument();
 
@@ -26,6 +29,11 @@ describe("Application", () => {
 
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
