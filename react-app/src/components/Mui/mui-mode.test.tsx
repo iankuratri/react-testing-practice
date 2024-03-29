725beadf7c0dffc5b988c4e171b5b486c04600bb
Ankur Atri <ankuratri.in@gmail.com>
2024-03-29 13:30:47 +0530

    Custom Render Function added  and used

--- react-app/src/components/Mui/mui-mode.test.tsx
+++ react-app/src/components/Mui/mui-mode.test.tsx
@@ -1,10 +1,9 @@
-import { screen, render } from "@testing-library/react";
+import { render, screen } from "./../../test-utils";
 import { MuiMode } from "./MuiMode";
-import { AppProviders } from "../../providers/AppProviders";
 
 describe("MuiMode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />, { wrapper: AppProviders });
+    render(<MuiMode />);
 
     const headingElement = screen.getByRole("heading");
     expect(headingElement).toHaveTextContent("dark mode");
