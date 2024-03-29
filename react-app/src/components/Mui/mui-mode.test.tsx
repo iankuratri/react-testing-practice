67899ab60fa451d169c9acabeb1327318a3852e8
Ankur Atri <ankuratri.in@gmail.com>
2024-03-29 13:15:38 +0530

    Providers example added

---
+++ react-app/src/components/Mui/mui-mode.test.tsx
@@ -0,0 +1,12 @@
+import { screen, render } from "@testing-library/react";
+import { MuiMode } from "./MuiMode";
+import { AppProviders } from "../../providers/AppProviders";
+
+describe("MuiMode", () => {
+  test("renders text correctly", () => {
+    render(<MuiMode />, { wrapper: AppProviders });
+
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toHaveTextContent("dark mode");
+  });
+});
